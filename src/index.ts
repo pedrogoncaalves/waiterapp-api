724433b6e6956c5b1449c17e375bb015d5723bd8
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-11-15 00:29:06 -0300

    creating models

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
 
-app.listen(3001, () => {
-    console.log('Server is running on http://localhost:3001');
-});
+
+
+mongoose.connect('mongodb://localhost:27017')
+    .then(() => {
+        const app = express();
+        app.listen(3001, () => {
+            console.log('Server is running on http://localhost:3001');
+        });
+
+    })
+    .catch(() => console.log('erro ao tentar conectar ao mongo'));
+
 
 
