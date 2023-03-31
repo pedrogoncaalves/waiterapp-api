9542b34633afcb3dbc11f0a5098d64e14e451c7d
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2023-03-31 15:46:46 -0300

    fixing last bug

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,12 @@ mongoose.connect('mongodb://localhost:27017')
     .then(() => {
         const app = express();
 
-        app.use((req, res) => {
+        app.use((req, res, next) => {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.setHeader('Access-Control-Allow-Methods', '*');
             res.setHeader('Access-Control-Allow-Headers', '*');
+
+            next();
         });
         app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
         app.use(express.json());
